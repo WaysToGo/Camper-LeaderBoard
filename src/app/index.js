1675b0aba99332e59cc41e6538af5e7fa23cd62f
WaysToGo <readytohackk@gmail.com>
2017-10-31 00:47:54 +0530

    after ratriving data without css

--- src/app/index.js
+++ src/app/index.js
@@ -12,23 +12,44 @@ class MyApp extends React.Component {
         }
 
     }
-    componentDidMount(){
-       
+    componentWillMount(){
         api.fetchCamperApi().then((result)=>{
-           this.setState(result);
+           this.setState({userList:result});
+         
         })
         
-        
     }
+//     componentDidUpdate(prevProps, prevState) {
+//   console.log(this.state.userList)
+//         }
+      
 
     render() {
-        return ( 
-        <div>
-
-            <h1> Welcome to react home page </h1>
-            <Header/> 
-             </div>
-        );
+if(this.state.userList!=null){
+        const users=this.state.userList.map((a,i)=>{
+            return(<table key={i}>
+            <tbody>
+                <tr>
+                    <td >{i}</td>
+                    <td >{a.username}<img src={a.img}/></td>
+                    <td >{a.alltime}</td>
+                    <td >{a.recent}</td>
+                </tr>
+                </tbody>
+            </table>);
+            })
+            console.log(users)
+            return ( 
+                <div>
+        
+                    <h1> Welcome to react home page </h1>
+                    <Header/>
+                <h1>{users}</h1> 
+                     </div>
+                );
+    }
+            else
+            return null
     }
 };
 
